a641bb1209964bfe44948cda5aae3ab1edd2acf7
Sainteyegor <Sainteyegor@users.noreply.github.com>
2020-04-08 16:45:41 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    => '',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDSJfZ1lof3I9Vt+6H2hwnxXU57XIjs2yZWim1y0ZWfAC/uYU1OohpkI6Ta1K0nbb2DP/e2MlYN51rjcu9vVWhvxOYtE4Z9Jor+QGWSZ5gHgoKVGasY5WgsGa7LW1hm/oHczAm+qMSqA47Atfc3Il4WESsMx486YgBmaG8zHkd/PXnWkvhctvUA1POHoRciBHtJpjwQHzYFMFib/Z6FCzo0Oxd54743wLOVs0n945z75iMSKrscjXUaGimWRsMdgZb+7buf0fUkpJDLK3b4R4jLMxnsMF8bTIgWzCqlva5HhwKcEmoY8k8cf3hmhmyK6QdzZnq4mpxLf6DCuuvZX2UX',
         }  
 }
